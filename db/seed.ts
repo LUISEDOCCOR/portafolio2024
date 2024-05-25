76a3c5ae33946f0e781b551e26fbaae22b532977
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2024-05-24 23:43:19 -0600

    feat: a prototype of a post

--- db/seed.ts
+++ db/seed.ts
@@ -7,6 +7,11 @@ export default async function seed() {
 			label: "Crea páginas web con HTML y CSS",
 			description: "Curso sobre cómo crear y maquetar páginas usando HTML y CSS",
 			href: "https://www.mastermind.ac/certificates/67103e6b5b66470480a38a5f773caed1"
+		},
+		{
+			label: "¡Linux: de Noob a Pro en 9 horas!",
+			description: "Sistemas operativos basados en Debian Linux.",
+			href: "https://www.mastermind.ac/certificates/33290ab05fc14ed99e72c73613469b96"
 		}
 	])
 }
